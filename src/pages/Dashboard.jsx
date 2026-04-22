eaa2207041fbb233a235c0ec96ecbf132402bb80
Pacolla15 <38581@raphaeldisanto.com.br>
2026-04-22 17:17:38 -0300

    Add files via upload

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,9 @@
 import styles from './Dashboard.module.css';
 import lmc_logo from '../assets/lmcplusv2.png'
+import casa from '../assets/casa.png'
+import tabela from '../assets/tabela.png'
+import user from '../assets/usuario.png'
+import diario from '../assets/diario.png'
 import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
 import { Subtitles } from 'lucide-react';
@@ -16,6 +20,34 @@ export function Dashboard() {
     return (
         <main className={styles.main}>
             <nav className={styles.vertical_bar}>
+                <button className={`${styles.menubtn} ${styles.inicio}`}>
+                    <img src={casa} width="40" height="40"/>
+                    <span>Início</span>
+                </button>
+
+                <div className={styles.area1}>
+                    <button className={`${styles.menubtn} ${styles.parent_btn}`}>
+                        <img src={tabela} width="40" height="40"/>
+                        <span>Programação</span>
+                    </button>
+                    <button className={styles.menubtn}>Criar nova</button>
+                    <button className={styles.menubtn}>Minhas Programações</button>
+                </div>
+
+                <button className={`${styles.menubtn} ${styles.parent_btn}`}>
+                    <img src={user} width="40" height="40"/>
+                    <span>Perfis</span>
+                </button>
+
+                <div className={styles.area2}>
+                    <button className={`${styles.menubtn} ${styles.parent_btn}`}>
+                        <img src={diario} width="40" height="40"/>
+                        <span>Guias</span>
+                    </button>
+                    <button className={styles.menubtn}>Uso de perfis</button>
+                    <button className={styles.menubtn}>Criação de tabela</button>
+                    <button className={styles.menubtn}>Inserções</button>
+                </div>
 
             </nav>
 
@@ -120,6 +152,22 @@ export function Dashboard() {
                     </div>
                 </div>
                 
+
+                <div className={styles.importar}>
+                    <div className={styles.importar_1}>
+                        <h1>Importar arquivo</h1>
+                        <h2>Importe uma tabela a partir de outro arquivo externo</h2>
+                    </div>
+
+                    <div className={styles.importar_2}>
+                        <h2>Arquivos suportados
+                            <p>.xlsx .tsv .xlsb .ods</p>
+                        </h2>
+                        <button>Importar</button>
+                    </div>
+                </div>
+
+
             </div>
 
 
