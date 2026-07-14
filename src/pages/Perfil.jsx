9935d49d4db640f174839a9b5d63516db5f760d4
Pacolla15 <38581@raphaeldisanto.com.br>
2026-07-14 12:56:17 -0300

    Add files via upload

---
+++ src/pages/Perfil.jsx
@@ -0,0 +1,64 @@
+import styles from './Perfil.module.css';
+import TopNavbar from '../components/TopNavbar.jsx';
+import SideNavbar from '../components/SideNavbar.jsx';
+import ContaNavbar from "../components/ContaNavbar.jsx";
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import imageperfil from '../assets/imageperfil.jfif';
+import lapis from '../assets/lapis.png';
+
+export function Perfil() {
+    const navigate = useNavigate();
+
+    const irHome = () => {
+        navigate('/Perfil');
+    };
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    
+    return(
+        <main className={styles.conta}>
+            <TopNavbar
+                menuOpen={menuOpen}
+                setMenuOpen={setMenuOpen}
+            />
+            <SideNavbar
+                menuOpen={menuOpen}
+            />
+
+            <div className={styles.area}>
+                <ContaNavbar/>
+
+                {/* area grid, separa form do perfil e foto de usuário */}
+                <div className={styles.perfilContainer}>
+                    
+                    {/* formulário do perfil */}
+                    <div className={styles.form}>
+                        <h1>Perfil</h1>
+
+                        <span className={styles.span}>Nome</span>
+                        <input type="text" placeholder='Digite seu nome'/>
+
+                        <span className={styles.span}>Senha</span>
+                        <input type="text" placeholder='Digite sua senha'/>
+        
+                        <span className={styles.span}>E-mail</span>
+                        <input type="text" placeholder='Email@email.com'/>
+
+                        <button>Salvar</button>
+                    </div>
+                    
+                    {/* imagem do perfil */}
+                    <div className={styles.perfilImageContainer}>
+                        <h2>Foto de Perfil</h2>
+                        <div className={styles.profilePictureHolder} style={{backgroundImage: `url(${imageperfil})`}}>
+                            {/* Fazer pegar foto de perfil aqui, n sabemos fazer! */}
+                            {/* <img src={imageperfil} alt="Foto de Perfil" className={styles.profilePicture} /> */}
+                            <button className={styles.editPicture} style={{backgroundImage: `url(${lapis})`}}></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
