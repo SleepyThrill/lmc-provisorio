2e42fd3f1c55600a4fa257c848bb90e78ce5af6b
Pacolla15 <38581@raphaeldisanto.com.br>
2026-07-14 12:58:28 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Login } from './pages/Login'
 import { Dashboard } from './pages/Dashboard'
+import { SideNavbar } from './components/SideNavbar'
+import { Perfil } from './pages/Perfil'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/SideNavbar" element={<SideNavbar />} />
+        <Route path="/Perfil" element={<Perfil />} /> 
       </Routes>
     </Router>
   )
