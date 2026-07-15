10f30849fe69dd27e29477ae4a3e3c85e07ff609
Pacolla15 <38581@raphaeldisanto.com.br>
2026-07-15 10:58:20 -0300

    Add files via upload

--- src/components/ContaNavbar.jsx
+++ src/components/ContaNavbar.jsx
@@ -5,20 +5,34 @@ import paleta from '../assets/paleta.png'
 import pessoa from '../assets/pessoa.png'
 import autenticacao from '../assets/autenticacao.png'
 
+import { useState } from "react";
+
 
 export function ContaNavbar(){
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return(
-        <div className={styles.ContaNavbar}>
+        <>
+            <button
+                className={`${styles.arrowButton} ${menuOpen ? styles.openArrow : ""}`}
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                {menuOpen ? "<" : ">"}
+            </button>
 
-            <div className={styles.areaTitulo}>
-                <span>Conta</span>
+            <div className={`${styles.ContaNavbar} ${menuOpen ? styles.open : ""}`}>
+
+                <div className={styles.areaTitulo}>
+                    <span>Conta</span>
+                </div>
+
+                <button className={styles.button}> <img src={usuario} width='30px' height='30px'/> Perfil</button>
+                <button className={styles.button}> <img src={cadeado} width='30px' height='30px'/>Acesso</button>
+                <button className={styles.button}> <img src={paleta} width='30px' height='30px'/>Aparência</button>
+                <button className={styles.button}> <img src={pessoa} width='30px' height='30px'/>Acessibilidade</button>
+                <button className={styles.button}> <img src={autenticacao} width='30px' height='30px'/>Autenticação</button>
             </div>
-
-            <button className={styles.button}> <img src={usuario} width='30px' height='30px'/> Perfil</button>
-            <button className={styles.button}> <img src={cadeado} width='30px' height='30px'/>Acesso</button>
-            <button className={styles.button}> <img src={paleta} width='30px' height='30px'/>Aparência</button>
-            <button className={styles.button}> <img src={pessoa} width='30px' height='30px'/>Acessibilidade</button>
-            <button className={styles.button}> <img src={autenticacao} width='30px' height='30px'/>Autenticação</button>
-        </div>
+        </>
     )
 }
